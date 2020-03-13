b5d3ae48a26d2624600df19a3f0b58c96378e58f
Muhammad Jbara <mohammad.jbara@gmail.com>
2020-03-14 01:31:58 +0200

    First Commit

--- Reporters/AppDelegate.swift
+++ Reporters/AppDelegate.swift
@@ -10,28 +10,23 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
+    // MARK: - Interstitial AppDelegate
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+//        FirebaseApp.configure()
+//        self.launchOptions = launchOptions
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+//            self.containerController = ContainerController(withDelegate: self as ContainerControllerDelegate)
+//            window.backgroundColor = UIColor.clear
+//            window.rootViewController = self.containerController
+            window.makeKeyAndVisible()
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
